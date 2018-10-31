4d6d3dd2fca735ae8462e9c2905e7a30ee0e3707
Thang Duong <cam.thang.duong@gmail.com>
2018-10-30 20:41:27 -0400

    Simple RegEx

--- Day 6/RegExample/Program.cs
+++ Day 6/RegExample/Program.cs
@@ -13,6 +13,30 @@ namespace RegExample
             if (match.Success) {
                 Console.WriteLine(match.Value);
             }
+
+            // First we see the iunput string
+            string input = "/content/alternative-1.aspx";
+            Console.WriteLine(input);
+
+            // Here we call Regex.Match
+            Match matched = Regex.Match(input, @"content/([A-Za-z0-9\-]+)\.aspx$", RegexOptions.IgnoreCase);
+
+            // Here we check the Match instance
+            if (match.Success) {
+                // Finally we ge the group value and display it
+                string key = matched.Groups[1].Value;
+                Console.WriteLine(key);
+            }
+
+            string value = "4 AND 5";
+            match = Regex.Match(value, @"\d");
+            if (match.Success) {
+                Console.WriteLine(match.Value);
+            }
+            match = match.NextMatch();
+            if (match.Success) {
+                Console.WriteLine(match.Value);
+            }
         }
     }
 }
